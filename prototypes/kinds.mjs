14d6eed97854ebe686b60c420f4daf7d1fbf83a7
Saleh <saleh@grasppe.com>
2018-12-09 06:03:47 -0500

    Add tokens experiment (testing symlinks)

---
+++ prototypes/kinds.mjs
@@ -0,0 +1,7 @@
+export const Kind = kind => Symbol.for(kind);
+export const TextKind = Kind('text');
+export const UnknownKind = Kind('');
+// export const SeparatorKind = Kind('separator');
+// export const EnclosureKind = Kind('enclosure');
+// export const OperatorKind = Kind('operator');
+// export const PunctuatorKind = Kind('punctuator');
