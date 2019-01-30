f42309a43ab8fb0ab2643990a2ffce2f50dde177
Saleh <saleh@grasppe.com>
2019-01-30 18:51:07 -0500

    Update markup.js

--- markup.js
+++ markup.js
@@ -3,7 +3,7 @@ import markup, {versions} from './lib/markup.js';
 
 // @SMotaal/tokenizer Implementation
 // import * as smotaalTokenizerParser from './packages/tokenizer/lib/parser.mjs';
-import smotaalTokenizerParser from './packages/@smotaal/tokenizer/examples/extended.mjs';
+import smotaalTokenizerParser from './packages/@smotaal/tokenizer/extended.mjs';
 
 versions.push(smotaalTokenizerParser);
 
