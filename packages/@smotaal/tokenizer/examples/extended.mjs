4ec08eda7a94add510c36b8d7facdf57b51b1e7e
Saleh <saleh@grasppe.com>
2019-01-30 18:53:27 -0500

    Update tokenizer examples

--- packages/@smotaal/tokenizer/examples/extended.mjs
+++
@@ -1,7 +0,0 @@
-import {Parser, extensions} from '../extended.mjs';
-
-const parser = new Parser();
-export const {modes, mappings} = parser;
-for (const id in extensions.modes) parser.register(extensions.modes[id]);
-
-export default parser;
