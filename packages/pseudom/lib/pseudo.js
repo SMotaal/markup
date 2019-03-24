ad590f7d9ed0dfb0cf5ab19e7ad20f75c1a211d2
Saleh <saleh@grasppe.com>
2019-03-24 14:35:22 -0400

    Publish pseudom v0.0.4

--- packages/pseudom/lib/pseudo.js
+++ packages/pseudom/lib/pseudo.js
@@ -1,1 +1,90 @@
-./pseudo.mjs
+const {assign, defineProperty} = Object;
+
+export const document = void null;
+
+export class Node {
+  get children() {
+    return defineProperty(this, 'children', {value: new Set()}).children;
+  }
+  get childElementCount() {
+    return (this.hasOwnProperty('children') && this.children.size) || 0;
+  }
+  get textContent() {
+    return (
+      (this.hasOwnProperty('children') && this.children.size && [...this.children].join('')) || ''
+    );
+  }
+  set textContent(text) {
+    this.hasOwnProperty('children') && this.children.size && this.children.clear();
+    text && this.children.add(new String(text));
+  }
+  appendChild(element) {
+    return element && this.children.add(element), element;
+  }
+  append(...elements) {
+    if (elements.length) for (const element of elements) element && this.children.add(element);
+  }
+  removeChild(element) {
+    element &&
+      this.hasOwnProperty('children') &&
+      this.children.size &&
+      this.children.delete(element);
+    return element;
+  }
+  remove(...elements) {
+    if (elements.length && this.hasOwnProperty('children') && this.children.size)
+      for (const element of elements) element && this.children.delete(element);
+  }
+}
+
+export class Element extends Node {
+  get innerHTML() {
+    return this.textContent;
+  }
+  set innerHTML(text) {
+    this.textContent = text;
+  }
+  get outerHTML() {
+    const {className, tag, innerHTML} = this;
+    return `<${tag}${(className && ` class="${className}"`) || ''}>${innerHTML || ''}</${tag}>`;
+  }
+  toString() {
+    return this.outerHTML;
+  }
+  toJSON() {
+    return this.toString();
+  }
+}
+
+export class DocumentFragment extends Node {
+  toString() {
+    return this.textContent;
+  }
+  toJSON() {
+    return (this.childElementCount && [...this.children]) || [];
+  }
+  [Symbol.iterator]() {
+    return ((this.childElementCount && this.children) || '')[Symbol.iterator]();
+  }
+}
+
+export class Text extends String {
+  toString() {
+    return encodeEntities(super.toString());
+  }
+}
+
+export const createElement = (tag, properties, ...children) => {
+  const element = assign(new Element(), {
+    tag,
+    className: (properties && properties.className) || '',
+    properties,
+  });
+  children.length && defineProperty(element, 'children', {value: new Set(children)});
+  return element;
+};
+
+export const createText = (content = '') => new Text(content);
+export const encodeEntity = entity => `&#${entity.charCodeAt(0)};`;
+export const encodeEntities = string => string.replace(/[\u00A0-\u9999<>\&]/gim, encodeEntity);
+export const createFragment = () => new DocumentFragment();
