0da846234c70e757dcf16cf9ac50c01191a7cc15
Saleh <saleh@grasppe.com>
2019-04-01 13:12:33 -0400

    Refactor `markup.experimental.js` and `tokenizer.api.js` from `browser/markup.js`

--- packages/@smotaal/tokenizer/browser/markup.experimental.js
+++ packages/@smotaal/tokenizer/browser/markup.experimental.js
@@ -1,5 +1,22 @@
-export * from './markup.js';
-import experimentalParser from '../tokenizer.experimental.extended.js';
-import {parsers} from './markup.js';
+// export * from './markup.js';
+import parser from '../tokenizer.experimental.extended.js';
+import {TokenizerAPI} from '../tokenizer.api.js';
 
-parsers.unshift(experimentalParser);
+import dom from '../extensions/dom.js';
+
+export let parsers, render, tokenize, warmup;
+
+export default ({parsers, render, tokenize, warmup} = new TokenizerAPI({
+  parsers: [parser],
+  render: (source, options, flags) => {
+    const fragment = options && options.fragment;
+
+    const debugging = flags && /\bdebug\b/i.test(typeof flags === 'string' ? flags : [...flags].join(' '));
+
+    debugging && console.info('render: %o', {render, source, options, flags, fragment, debugging});
+
+    fragment && (fragment.logs = debugging ? [] : undefined);
+
+    return dom.render(tokenize(source, options, flags), fragment);
+  },
+}));
---
+++ packages/@smotaal/tokenizer/tokenizer.api.js
@@ -0,0 +1,141 @@
+﻿/**
+ *
+ */
+export const TokenizerAPI = Object.setPrototypeOf(
+  class TokenizerAPI {
+    /**
+     * @param {Partial<{parsers: Parser[]}>} [options]
+     */
+    constructor() {
+      const [
+        {
+          parsers = [],
+          State = TokenizerState,
+          tokenize = (source, options = {}, flags) => {
+            const state = new State({options, flags: {}});
+            const variant = !options.variant ? 1 : parseInt(options.variant);
+            const {[variant >= 1 && variant <= parsers.length ? variant - 1 : (options.variant = 0)]: parser} = parsers;
+            this.lastVariant === (this.lastVariant = variant) ||
+              variant <= parsers.length ||
+              console.warn(
+                '[tokenize‹parser›] Variant %O[%d] out of bounds — using default parser: %o',
+                parsers,
+                variant,
+                parser.MODULE_URL || {parser},
+              );
+            options.tokenize = parser.tokenize;
+            if (flags && (flags.length > 0 || flags.size > 0)) {
+              typeof flags === 'string' || (flags = [...flags].join(' '));
+              /\bwarmup\b/i.test(flags) && (state.flags.warmup = true);
+              /\bdebug\b/i.test(flags) && (state.flags.debug = true);
+            }
+
+            let returned = UNSET;
+            try {
+              this.lastParser === (this.lastParser = parser) ||
+                console.info('[tokenize‹parser›]: %o', parser.MODULE_URL || {parser});
+              return (returned = parser.tokenize(source, state));
+            } finally {
+              returned !== UNSET || !state.flags.debug || console.info('[tokenize‹state›]: %o', state);
+            }
+          },
+
+          warmup = (source, options, flags) => {
+            // Object.defineProperty(options, 'warmup', {value: true});
+            const key = (options && JSON.stringify(options)) || '';
+            let cache = (this.cache || (this.cache = new Map())).get(key);
+            cache || this.cache.set(key, (cache = new Set()));
+            if (!cache.has(source)) {
+              flags = `warmup ${(flags &&
+                (flags.length > 0 || flags.size > 0) &&
+                (typeof flags === 'string' || flags instanceof String ? flags : [...flags].join(' '))) ||
+                ''}`;
+              for (const item of tokenize(source, options, flags));
+            }
+            cache.add(source);
+          },
+
+          render,
+        } = {},
+      ] = arguments;
+
+      Object.defineProperties(this, {
+        tokenize: {get: () => tokenize},
+        warmup: {get: () => warmup},
+        render: {get: () => render},
+        parsers: {get: () => parsers},
+      });
+    }
+  }.prototype,
+  null,
+).constructor;
+
+const TokenizerState = Object.setPrototypeOf(
+  class State {
+    constructor(...properties) {
+      Object.assign(this, ...properties);
+    }
+  }.prototype,
+  null,
+).constructor;
+
+const UNSET = Symbol('');
+
+/** @typedef {import('./lib/parser.js').Parser} Parser */
+
+// export const createAPI = ({
+//   parsers = [],
+
+//   State: TokenizerState,
+
+//   tokenize = (source, options = {}, flags) => {
+//     const state = new TokenizerState({options, flags: {}});
+//     const variant = !options.variant ? 1 : parseInt(options.variant);
+//     const {[variant >= 1 && variant <= parsers.length ? variant - 1 : (options.variant = 0)]: parser} = parsers;
+//     this.lastVariant === (this.lastVariant = variant) ||
+//       variant <= parsers.length ||
+//       console.warn(
+//         '[tokenize‹parser›] Variant %O[%d] out of bounds — using default parser: %o',
+//         parsers,
+//         variant,
+//         parser.MODULE_URL || {parser},
+//       );
+//     options.tokenize = parser.tokenize;
+//     if (flags && (flags.length > 0 || flags.size > 0)) {
+//       typeof flags === 'string' || (flags = [...flags].join(' '));
+//       /\bwarmup\b/i.test(flags) && (state.flags.warmup = true);
+//       /\bdebug\b/i.test(flags) && (state.flags.debug = true);
+//     }
+
+//     let returned = UNSET;
+//     try {
+//       this.lastParser === (this.lastParser = parser) ||
+//         console.info('[tokenize‹parser›]: %o', parser.MODULE_URL || {parser});
+//       return (returned = parser.tokenize(source, state));
+//     } finally {
+//       returned !== UNSET || !state.flags.debug || console.info('[tokenize‹state›]: %o', state);
+//     }
+//   },
+
+//   warmup = (source, options, flags) => {
+//     // Object.defineProperty(options, 'warmup', {value: true});
+//     const key = (options && JSON.stringify(options)) || '';
+//     let cache = (this.cache || (this.cache = new Map())).get(key);
+//     cache || this.cache.set(key, (cache = new Set()));
+//     if (!cache.has(source)) {
+//       flags = `warmup ${(flags &&
+//         (flags.length > 0 || flags.size > 0) &&
+//         (typeof flags === 'string' || flags instanceof String ? flags : [...flags].join(' '))) ||
+//         ''}`;
+//       for (const item of tokenize(source, options, flags));
+//     }
+//     cache.add(source);
+//   },
+
+//   render,
+// }) => ({
+//   parsers,
+//   tokenize,
+//   warmup,
+//   render,
+// });
