bd797dfdcbbd815fd8d5f4ac7d4d564a9c2b569b
Saleh <saleh@grasppe.com>
2019-04-02 12:36:09 -0400

    Linting

--- packages/@smotaal/tokenizer/tokenizer.experimental.extended.js
+++ packages/@smotaal/tokenizer/tokenizer.experimental.extended.js
@@ -1,13 +1,9 @@
 export * from './tokenizer.experimental.js';
-
 import {Parser} from './tokenizer.experimental.js';
 import {modes} from './extensions/extensions.js';
 
-const parser = new Parser();
-parser.MODULE_URL = import.meta.url;
+const parser = Object.assign(new Parser(), {MODULE_URL: import.meta.url});
 for (const id in modes) parser.register(modes[id]);
 
 export {modes};
-
-
 export default parser;
