b076457912656cddb345302f0ff1a26b064907a9
Saleh <saleh@grasppe.com>
2019-04-02 11:30:11 -0400

    Create regexp-grammar.js

---
+++ packages/@smotaal/grammars/regexp/regexp-grammar.js
@@ -0,0 +1,22 @@
+﻿import {Symbols, Closures} from '../common/helpers.js';
+
+export const regexp = Object.defineProperties(
+  ({syntax} = regexp.defaults) => ({
+    syntax,
+    // comments: Closures.from('/*…*/'),
+    closures: Closures.from('{…} (…) […]'),
+    // quotes: Symbols.from(`' "`),
+    assigners: Symbols.from(`?: ?=`),
+    combinators: Symbols.from('+? *? + *'),
+    // nonbreakers: Symbols.from(`\\`),
+    // breakers: Symbols.from(', ;'),
+    matcher: /(\s+)|(\\(?:(?:\\\\)*\\|[^\\\s\n])?|\(|\)|\[|\]|"|'|\{|\}|,|\?:|\?=|\+\?|\*\?|\+|\*|\?)/g,
+    matchers: {
+      // quote: /(\n)|(\\(?:(?:\\\\)*\\|[^\\\s])?|\*\/|`|"|'|\$\{)/g,
+      // comment: /(\n)|(\*\/|\b(?:[a-z]+\:\/\/|\w[\w\+\.]*\w@[a-z]+)\S+|@[a-z]+)/gi,
+    },
+  }),
+  {
+    defaults: {value: {syntax: 'regexp'}},
+  },
+);
