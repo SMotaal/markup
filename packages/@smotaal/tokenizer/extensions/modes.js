774c8df61c11f3599f8153abfeaaef965e122790
Saleh <saleh@grasppe.com>
2019-04-02 11:30:41 -0400

    Add typescript-grammar to modes

--- packages/@smotaal/tokenizer/extensions/modes.js
+++ packages/@smotaal/tokenizer/extensions/modes.js
@@ -2,4 +2,5 @@ export {css} from '../../grammars/css/css-grammar.js';
 export {html} from '../../grammars/html/html-grammar.js';
 export {markdown} from '../../grammars/markdown/markdown-grammar.js';
 export {javascript} from '../../grammars/javascript/javascript-grammar.js';
+export {typescript} from '../../grammars/typescript/typescript-grammar.js';
 export {mjs, cjs, esx} from '../../grammars/javascript/javascript-extended-grammar.js';
