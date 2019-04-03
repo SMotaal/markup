cfe0b011930f467d53b1ae8cb0cb4599180bd420
Saleh <saleh@grasppe.com>
2019-04-03 08:00:54 -0400

    Clear [trash]

--- [trash]/prototypes/token-kind.mjs
+++
@@ -1,7 +0,0 @@
-export const Kind = kind => Symbol.for(kind);
-export const TextKind = Kind('text');
-export const UnknownKind = Kind('');
-// export const SeparatorKind = Kind('separator');
-// export const EnclosureKind = Kind('enclosure');
-// export const OperatorKind = Kind('operator');
-// export const PunctuatorKind = Kind('punctuator');
