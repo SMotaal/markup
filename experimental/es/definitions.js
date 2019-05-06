fc0381f85d18cc0b09042cbd5247a441662f1275
Saleh <saleh@grasppe.com>
2019-05-05 20:10:36 -0400

    Update experimental match

---
+++ experimental/es/definitions.js
@@ -0,0 +1,77 @@
+﻿import * as entities from './entities.js';
+
+const symbols = {};
+
+const symbol = (description, key) => (symbols[key || description] = Symbol(description));
+
+const identities = {
+  UnicodeIDStart: 'ECMAScript.UnicodeIDStart',
+  UnicodeIDContinue: 'ECMAScript.UnicodeIDContinue',
+  HexDigits: 'ECMAScript.HexDigits',
+  CodePoint: 'ECMAScript.CodePoint',
+  ControlEscape: 'ECMAScript.ControlEscape',
+  RestrictedWord: 'ECMAScript.RestrictedWord',
+  FutureReservedWord: 'ECMAScript.FutureReservedWord',
+  Keyword: 'ECMAScript.Keyword',
+};
+
+const goals = {
+  [symbol('ECMAScriptGoal')]: {openers: ['{', '(', '[', entities.GraveAccent]},
+  [symbol('TemplateLiteralGoal')]: {
+    openers: ['${'],
+    closer: entities.GraveAccent,
+    type: 'quote',
+    matcher: /\\.|(\x60|$\{)/g,
+  },
+  [symbol('FaultGoal')]: {openers: [], closer: '', type: 'fault'},
+};
+
+const groups = {
+  '{': {opener: '{', closer: '}'},
+  '${': {opener: '${', closer: '}', type: 'span'},
+  '(': {opener: '(', closer: ')'},
+  '[': {opener: '[', closer: ']'},
+  [entities.GraveAccent]: {
+    opener: entities.GraveAccent,
+    closer: entities.GraveAccent,
+    goal: symbols.TemplateLiteralGoal,
+  },
+};
+
+{
+  const {freeze, getOwnPropertySymbols} = Object;
+
+  for (const identity of [
+    'UnicodeIDStart',
+    'UnicodeIDContinue',
+    'HexDigits',
+    'CodePoint',
+    'ControlEscape',
+    'RestrictedWord',
+    'FutureReservedWord',
+    'Keyword',
+  ]) {
+    symbol((identities[identity] = `ECMAScript.${identity}`), identity);
+  }
+
+  for (const symbol of getOwnPropertySymbols(goals)) {
+    goals[symbol].name = (goals[symbol].symbol = symbol).description.replace(/Goal$/, '');
+    freeze(goals[symbol]);
+  }
+
+  freeze(goals);
+  freeze(groups);
+  freeze(identities);
+  freeze(symbols);
+}
+
+export {entities, identities, goals, groups, symbols};
+
+// UnicodeIDStart: Symbol(identities.UnicodeIDStart),
+// UnicodeIDContinue: Symbol(identities.UnicodeIDContinue),
+// HexDigits: Symbol(identities.HexDigits),
+// CodePoint: Symbol(identities.CodePoint),
+// ControlEscape: Symbol(identities.ControlEscape),
+// RestrictedWord: Symbol(identities.RestrictedWord),
+// FutureReservedWord: Symbol(identities.FutureReservedWord),
+// Keyword: Symbol(identities.Keyword),
--- experimental/es/playground.js
+++ experimental/es/playground.js
@@ -1,169 +1,42 @@
 ﻿import bootstrap from '../matcher/matcher.js';
 import {createTokenFromMatch} from '../matcher/helpers.js';
-import {Matcher} from '/modules/matcher/matcher.js';
-import * as entities from './entities.js';
+// import {Matcher} from '/modules/matcher/matcher.js';
+import {matcher} from './matcher.js';
+// import {entities, identities, goals, groups, symbols} from './definitions.js';
 
-const symbols = {
-  UnicodeIDStart: Symbol('UnicodeIDStart'),
-  UnicodeIDContinue: Symbol('UnicodeIDContinue'),
-  HexDigits: Symbol('HexDigits'),
-  CodePoint: Symbol('CodePoint'),
-  ControlEscape: Symbol('ControlEscape'),
-  ReservedWord: Symbol('ReservedWord'),
-  Keyword: Symbol('Keyword'),
-  ECMAScriptGoal: Symbol('ECMAScriptGoal'),
-  TemplateLiteralGoal: Symbol('TemplateLiteralGoal'),
-  FaultGoal: Symbol('FaultGoal'),
-};
-
-const groups = {
-  '{': {opener: '{', closer: '}'},
-  '${': {opener: '${', closer: '}', type: 'span'},
-  '(': {opener: '(', closer: ')'},
-  '[': {opener: '[', closer: ']'},
-  '`': {opener: '`', closer: '`', goal: symbols.TemplateLiteralGoal},
-};
-
-const goals = {
-  [symbols.ECMAScriptGoal]: {openers: ['{', '(', '[', '`']},
-  [symbols.TemplateLiteralGoal]: {openers: ['${'], closer: '`', type: 'quote', matcher: /\\.|(\x60|$\{)/g},
-  [symbols.FaultGoal]: {openers: [], closer: '', type: 'fault'},
-};
-
-const {[symbols.ECMAScriptGoal]: ECMAScriptGoal, [symbols.FaultGoal]: FaultGoal} = goals;
-
-const open = (text, state) => {
-  if (!(state.goal || ECMAScriptGoal).openers.includes(text)) return;
-  const group = groups[text];
-  if (!group || !group.opener || !group.closer) return;
-  state.groups || (state.groups = []);
-  state.groups.closers || (state.groups.closers = []);
-  state.groups.closers.splice(state.groups.push(group) - 1, state.groups.closers.length, group.closer);
-  state.goal = (group.goal && goals[group.goal]) || undefined;
-  return 'opener';
-};
-
-const close = (text, state) => {
-  if (state.goal && state.goal.closer !== text) return state.goal.type;
-  const index = state.groups && state.groups.closers ? state.groups.closers.lastIndexOf(text) : -1;
-  if (index === -1 || index !== state.groups.length - 1) return (state.goal = FaultGoal).type;
-  state.groups.splice(index, state.groups.length);
-  state.groups.closers.splice(index, state.groups.closers.length);
-  const group = index && state.groups[state.groups.length - 1];
-  state.goal = (group && group.goal && goals[group.goal]) || undefined;
-  return 'closer';
-};
-
-const toggle = (text, state) =>
-  state.groups && state.groups.length && state.groups.closers[state.groups.length - 1] === text
-    ? close(text, state)
-    : open(text, state);
-
-const matcher = Matcher.define(
-  entity => Matcher.sequence`
-    (?=\s+)(?:
-      ^(?:
-        (${entity('whitespace')}\s+)?$(${entity('break')}\n)?|
-        (${entity('inset')}(${entity('whitespace')}\s+))
-      )|
-      (${entity((text, entity, match) => {
-        // TODO: Consider dropping /m flag
-        match.idenity = 'break';
-        const state = match.matcher.state;
-        state.goal !== FaultGoal || (state.goal = undefined);
-      })}\n)|
-      (${entity('whitespace')}\s+)
-    )|
-    \\(${entity('sequence')}
-      x(${entity(symbols.HexDigits)}[${entities.HexDigit}]{2})|
-      u(${entity(symbols.HexDigits)}[${entities.HexDigit}]{4})|
-      u\{(${entity(symbols.CodePoint)}[${entities.HexDigit}]{1,4})\}|
-      (${entity(symbols.ControlEscape)}f|n|r|t|v)|
-      c(${entity(symbols.ControlLetter)}[${entities.ControlLetter}])|
-      (${entity(symbols.CharacterEscape)}.)
-    )|
-    (?:
-      (${entity('comment')}\/\/.*|\/\*[^]*?\*\/)|
-      (${entity('quote')}
-        "(?:[^\\"\n]+|\\.)*(?:"|$)|
-        '(?:[^\\'\n]+|\\.)*(?:'|$)|
-        (${entity((text, entity, match) => {
-          (match.identity = toggle(text, match.matcher.state))
-            ? (match.punctuator = text)
-            : (match.identity = 'sequence');
-        })}${'`'})
-      )|
-      (
-        \$\{|\{|\(|\[
-        ${entity((text, entity, match) => {
-          (match.identity = open(text, match.matcher.state))
-            ? (match.punctuator = text)
-            : (match.identity = 'sequence');
-        })}
-      )|
-      (
-        \}|\)|\]
-        ${entity((text, entity, match) => {
-          (match.identity = close(text, match.matcher.state))
-            ? (match.punctuator = text)
-            : (match.identity = 'sequence');
-        })}
-      )|
-      (${entity('operator')}
-        ,|;|\.\.\.|\.|:|\?|=>|
-        \+\+|--|
-        \+=|-=|\*\*=|\*=|\/=|
-        &&|&=|&|\|\||\|=|\||%=|%|\^=|\^|~=|~|
-        <<=|<<|<=|<|>>>=|>>>|>>=|>>|>=|>|
-        !==|!=|!|===|==|=|
-        \+|-|\*\*|\*|(${entity((text, entity, match) => {
-          // TODO: Solidus and such
-          //   - regexp likely matches /[^+*?/\n][^\n]*)\/
-          match.idenity = 'operator';
-          match.punctuator = text;
-          const state = match.matcher.state;
-          state.goal !== FaultGoal || (state.goal = undefined);
-          // console.log({text, entity, ...match.matcher.state}, match.input.slice(matcher.index, matcher.index + 10));
-        })}\/)
-      )
-    )(${
-      // TODO: Come up with a less hacky way to do this
-      entity((text, entity, match) => {
-        match.punctuator || match.identity !== 'operator' || (match.punctuator = match[0]);
-        // console.log(text, entity, match);
-      })
-    })|
-    \b(${entity('keyword')}
-      (${entity(symbols.ReservedWord)}
-      abstract enum interface package namespace declare type module public protected)|
-      (${entity(symbols.Keyword)}
-      arguments|as|async|await|break|case|catch|class|
-      export|const|continue|private|debugger|default|
-      delete|do|else|export|extends|finally|for|from|
-      function|get|if|import|in|instanceof|let|new|of|
-      return|set|static|super|switch|this|throw|try|
-      typeof|var|void|while|with|yield)
-    )\b|
-    (${entity('identifier')}
-      (${entity(symbols.UnicodeIDStart)}[_$${entities.ID_Start}])
-      (${entity(symbols.UnicodeIDContinue)}[_$\u200c\u200d${entities.ID_Continue}\u034f]+)?
-    )|
-    (${entity('sequence')}.)
-  `,
-  'mgu',
-);
+const SP = `\u2420`;
+const HT = `\u2409`;
 
 export default bootstrap(matcher, {
   syntax: 'es',
+  initializeState: state => {
+    (state.groups = []).closers = [];
+    state.lineOffset = state.lineIndex = 0;
+    state.lineFault = false;
+  },
   createToken: (match, state) => {
     const token = createTokenFromMatch(match);
     const {type} = token;
-    const goal = state.goal;
+    const {goal, lineIndex, lineOffset = (state.lineOffset = 0)} = state;
+
     (goal &&
       goal.type &&
-      (type === 'closer' || type === 'opener' || (token.hint = `in-${(token.type = goal.type)}`))) ||
-      (match.punctuator && (token.punctuator = token.type));
+      (type === 'closer' || type === 'opener' || (
+        // token.punctuator && (console.log(token)),
+        token.hint = `in-${(token.type = goal.type)}`
+      ))) ||
+      (match.punctuator && (token.punctuator = type));
+
+    token.columnNumber = match.index - (lineOffset || -1);
+    token.lineNumber = lineIndex + 1;
+    token.goal = (goal || matcher.goal).name;
+    token.hint = `${token.hint || ''} [${token.goal}:${token.lineNumber}:${token.columnNumber}]`;
+    // ${(type !== 'inset' && token.inset && token.inset.replace(/\t/g, HT).replace(/\s/g, SP)) || ''}
+
+    // Manually update lineNumber for multi-line strings and comments
+    token.breaks && type !== 'break' && (state.lineIndex += token.breaks);
+
     return token;
   },
-  sourceURL: './playground.js',
+  sourceURL: './matcher.js',
 });
