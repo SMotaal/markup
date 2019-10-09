749c21050c54442a8392a117e997987c5d2ac115
Saleh Abdel Motaal <saleh@grasppe.com>
2019-10-09 19:55:55 -0400

    Add `pseudom-node-boilerplate` in tests

---
+++ packages/pseudom/tests/pseudom-node-boilerplate/index.js
@@ -0,0 +1,6 @@
+﻿// Set options as a parameter, environment variable, or rc file.
+require = require('esm')(module, {
+  // detect files with import, import.meta, export, "use module", or .mjs as ESM
+  mode: 'auto',
+});
+module.exports = require('./index.mjs');
---
+++ packages/pseudom/tests/pseudom-node-boilerplate/index.mjs
@@ -0,0 +1,40 @@
+﻿import * as pseudom from '@smotaal/pseudom';
+
+// Log the APIs imported from pseudom
+console.log({pseudom});
+
+{
+  // Create a DOM-less fragment in Node.js
+  const fragment = pseudom.createFragment();
+
+  // Append some DIV elements
+  fragment.append(
+    pseudom.createElement('div', {
+      dataset: {usingPropertiesArgument: true},
+      className: 'div-with-class-name-property',
+    }),
+    Object.assign(pseudom.createElement('div'), {
+      dataset: {usingObjectDotAssign: true},
+      className: 'div-with-assigned-class-name',
+    }),
+  );
+
+  // Log a newline just to space things out
+  console.log('\n');
+
+  // Log the element objects of the document fragment
+  console.log(
+    'fragment = ',
+    // NOTE: It's iterable so we are making an [... array] to
+    //       see all the elements in the console
+    [...fragment],
+  );
+
+  // Log a newline dido
+  console.log('\n');
+
+  // Log the reparsed JSON object which is how this HTML
+  //   will look when it is serialized and passed from
+  //   a worker or the backend before hittng the real DOM
+  console.log('json', JSON.parse(JSON.stringify(fragment)));
+}
