e092f55b2b43e56d72a4ac5a88631eefce42bfec
Saleh Abdel Motaal <saleh@grasppe.com>
2019-10-24 19:00:32 -0400

    Bake

--- packages/tokenizer/dist/tokenizer.experimental.es.standalone.js
+++ packages/tokenizer/dist/tokenizer.experimental.es.standalone.js
@@ -1098,8 +1098,8 @@ const createToken = (match, state) => {
 
       goal: currentGoal,
       group: contextGroup,
-      // state,
-      // context: tokenContext,
+      state,
+      context: tokenContext,
     };
   }
   /* Context */
@@ -1989,6 +1989,7 @@ const matcher = (ECMAScript =>
               : state.lastAtom === undefined ||
                 state.lastAtom.type === 'delimiter' ||
                 state.lastAtom.type === 'breaker' ||
+                state.lastAtom.text === '=>' ||
                 (state.lastAtom.type === 'operator'
                   ? state.lastAtom.text !== '++' && state.lastAtom.text !== '--'
                   : state.lastAtom.type === 'closer'
