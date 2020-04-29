434dd613e87657b4253e37fd3ed5e92d0e0ce031
Saleh Abdel Motaal <saleh@smotaal.io>
2020-04-29 17:12:39 -0400

    Refactor experimental/es/example

--- experimental/es/playground.js
+++ experimental/es/playground.js
@@ -6,7 +6,7 @@ import.meta.url.includes('/es/playground.js') && (mode.USE_CONSTRUCTS = true);
  * @param {import('/markup/packages/tokenizer/lib/api').API} markup
  */
 export default ((
-  sourceURL = './es-matcher.js',
+  sourceURL = './example',
   sourceType = 'es',
   resolveSourceType = (defaultType, {sourceType, resourceType, options}) => {
     if (!sourceType && (resourceType === 'javascript' || resourceType === 'octet')) return 'es';
